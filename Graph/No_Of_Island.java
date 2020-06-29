fed71524ef4040995622fb1264df5e4a6cc073f9
chirag <cr.tagadiya@gmail.com>
2020-06-28 22:23:39 -0400

    No island

--- Graph/No_Of_Island.java
+++ Graph/No_Of_Island.java
@@ -1,4 +1,30 @@
 /*
+    Given a 2d grid map of '1's (land) and '0's (water), 
+    count the number of islands. An island is surrounded by water
+     and is formed by connecting adjacent lands horizontally or vertically. 
+     You may assume all four edges of the grid are all surrounded by water.
+
+    Example 1:
+
+        Input:
+            11110
+            11010
+            11000
+            00000
+
+        Output:
+             1
+    Example 2:
+
+    Input:
+        11000
+        11000
+        00100
+        00011
+
+    Output:
+         3
+
     Space complexity : O(M*N)
     Time complexity :O(M*N)
     is worked on leetcode :  YeS
@@ -54,7 +80,7 @@ public class No_Of_Island {
 
     /*
     DFS Solution
-    
+
     int m;
     int n;
     int [][] dirs = {{0,1},{0,-1},{-1,0},{1,0}};
