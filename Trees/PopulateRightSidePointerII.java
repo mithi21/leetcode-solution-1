e03f267ab9c170ee2fe864d925bed1bf04a9adb8
Chirag Tagadiya <cr.tagadiya@gmail.com>
2021-03-15 11:09:02 -0400

    Optimized code with Constant space

--- Trees/PopulateRightSidePointerII.java
+++ Trees/PopulateRightSidePointerII.java
@@ -5,13 +5,11 @@ class Node {
     public Node left;
     public Node right;
     public Node next;
-
     public Node() {}
     
     public Node(int _val) {
         val = _val;
     }
-
     public Node(int _val, Node _left, Node _right, Node _next) {
         val = _val;
         left = _left;
@@ -22,8 +20,7 @@ class Node {
 */
 
 /**
-Given a binary tree
-
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
 struct Node {
   int val;
   Node *left;
@@ -31,28 +28,24 @@ struct Node {
   Node *next;
 }
 Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
-
 Initially, all next pointers are set to NULL.
-
- 
-
-Follow up:
-
+/**
 You may only use constant extra space.
 Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
-
-Input: root = [1,2,3,4,5,null,7]
-Output: [1,#,2,3,#,4,5,7,#]
-Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
  
-
 Constraints:
-
-The number of nodes in the given tree is less than 6000.
--100 <= node.val <= 100
-
+The number of nodes in the given tree is less than 4096.
+-1000 <= node.val <= 1000
+Time complexity : O(N)
+space complexity : O(N)
+is worked on leetccode : YES
 **/
-
+  
+ 
+/**
 class Solution {
     List<List<Node>> levels;
     public Node connect(Node root) {
@@ -96,4 +89,57 @@ class Solution {
     }
     
    
+  
 }
+**/
+
+
+// More Optimized code  discuss in class 
+// with constant space
+// utilizing pointer idea
+// connect the level below root by next pointer and then use next pointers to get the connection
+// worked on Leetcode  : YES
+// recursive Space : O(Depth of Tree)
+// time xomplexity : O(N)
+// additional space : O(1)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        if(root.left != null) {
+            root.left.next  = root.right;
+        }
+        
+        if(root.next != null && root.right != null) {
+            
+            root.right.next = root.next.left;
+        }
+        
+        connect(root.left);
+        connect(root.right);
+        
+        return root;
+        
+    }
+}
+
